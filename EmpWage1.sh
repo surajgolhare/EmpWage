0c24291b0a792553b7803d80b449c60be16c0165
unknown <you@example.com>
2020-06-02 14:24:22 +0530

    welocme to git bash

--- EmpWage1.sh
+++ EmpWage1.sh
@@ -1,24 +1,29 @@
 #!/bin/bash -x
-isparttime=1;
-isfulltime=2;
+isPartTime=1;
+isFullTime=2;
 totalSalary=0;
-empratehr=20;
 numWorkingDays=20;
-
-for (( day=1; day<=$numWorkingDays; Day++ ))
-do
-empcheck=$((RANDOM%3));
-case $empcheck in
-       $isfulltime)
-                emphrs=8
-                ;;
-        $isparttime)
-                 emphrs=4
-                  ;;
-         *)
-           emphrs=0
-              ;;
+function funk()
+{
+      local empcheck
+    case  $empCheck in
+                    $isFullTime)
+                          empHrs=8
+                           ;;
+                    $isPartTime)
+                          empHrs=4
+                            ;;
+                         *)
+                           empHrs=0
+                            ;;
 esac
-      salary=$(($emphrs*$empratehr));
-      totalSalary=$(($totalSalary+$salary));
+ echo $empHrs
+}
+for  (( Day=1; day<=$numworkingDays; Day++ ))
+do
+   empcheck=$(( RANDOM % 3));
+   empHrs=$(( funk  $($empCheck) );
+Salary=$(( $empHrs * $empRateperHr ));
+totalSalary=$(( $totalSalary + $salary))
 done
+
