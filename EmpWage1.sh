f9f98b644a362bb3a4129e9d3bf75e2c0d7b17a2
unknown <you@example.com>
2020-06-02 14:06:16 +0530

    welocme to git bash

--- EmpWage1.sh
+++ EmpWage1.sh
@@ -1,13 +1,24 @@
 #!/bin/bash -x
-echo "Welcome to employee Wage computation Program"
-isPresent=$(( RANDOM % 2 ))
-if [ $isPresent -eq 1 ]
-then
-        echo "present"
-elif [ $isPresent -eq 0 ]
-then
-        echo "Absent"
-fi
+isparttime=1;
+isfulltime=2;
+totalSalary=0;
+empratehr=20;
+numWorkingDays=20;
 
-
-
+for (( day=1; day<=$numWorkingDays; Day++ ))
+do
+empcheck=$((RANDOM%3));
+case $empcheck in
+       $isfulltime)
+                emphrs=8
+                ;;
+        $isparttime)
+                 emphrs=4
+                  ;;
+         *)
+           emphrs=0
+              ;;
+esac
+      salary=$(($emphrs*$empratehr));
+      totalSalary=$(($totalSalary+$salary));
+done
